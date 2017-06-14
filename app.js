13882d6248f4b72b68ea55b0fbdd996dc932d134
Maria <tecnologiascalavera@gmail.com>
2017-06-13 22:59:40 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,120 @@
+//require('./dbconnection.js').open();
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+app.use(bodyParser());
+
+//coneccion a base de datos
+
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/calavera');
+//mongoose.connection.on("connected",function(){
+//  console.log('mongose conected to mongodb://localhost:27017/calavera');
+//});
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//
+
+var userSchema = new Schema({
+  nombre: { type: String, required: true },
+  correo: { type: String, required: true },
+  mensaje: { type: String, required: true },
+  momento: { type: String, required: true },
+  lugar: { type: String, required: true },
+});
+
+var reporte = mongoose.model('reporte', userSchema);
+module.exports = reporte;
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+//parser no seguro
+app.post('/', function(req, res) {
+
+    var name = req.body.user_name;
+    var message = req.body.user_message;
+    var email = req.body.user_mail;
+    var place = req.body.usercoord;
+    var time = req.body.tiempo;
+
+    //
+    var newUser = new reporte({
+      nombre:name.toString() ,
+      correo: message.toString() ,
+      mensaje: email.toString() ,
+      momento: place.toString() ,
+      lugar: time.toString() ,
+    });
+
+    //
+
+    console.log('you posted: First Name: ');
+ res.send(name + ' ; ' + message + ';' +email+';'+place +';'+time);
+
+ newUser.save(function(err) {
+   if (err) throw err;
+
+   console.log('User created!');
+ });
+
+});
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.listen(3000);
+
+
+module.exports = app;
